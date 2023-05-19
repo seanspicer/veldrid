830fff7d1635922c569fb95f2b8792113fc0654d
Dan Balasescu <smoogipoo@smgi.me>
2023-05-19 17:47:12 +0900

    Add support for macOS CVDisplayLink under Metal

---
+++ src/Veldrid/MTL/MTLCVDisplayLink.cs
@@ -0,0 +1,35 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using Veldrid.MetalBindings;
+
+namespace Veldrid.MTL
+{
+    internal unsafe class MTLCVDisplayLink : IMTLDisplayLink
+    {
+        public event Action Callback;
+
+        private CVDisplayLink _displayLink;
+        private CVDisplayLinkOutputCallbackDelegate _cvDisplayLinkCallbackHandler;
+
+        public MTLCVDisplayLink()
+        {
+            _cvDisplayLinkCallbackHandler = OnCallback;
+            _displayLink = CVDisplayLink.CreateWithActiveCGDisplays();
+            _displayLink.SetOutputCallback(_cvDisplayLinkCallbackHandler, IntPtr.Zero);
+            _displayLink.Start();
+        }
+
+        private int OnCallback(CVDisplayLink displaylink, CVTimeStamp* innow, CVTimeStamp* inoutputtime, long flagsin, long flagsout, IntPtr userdata)
+        {
+            Callback?.Invoke();
+            return 0;
+        }
+
+        public void Dispose()
+        {
+            _displayLink.Release();
+        }
+    }
+}
