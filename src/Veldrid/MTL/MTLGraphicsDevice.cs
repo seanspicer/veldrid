b9f3d06e4da5eb61af6ec99f393b81e80655740c
Dean Herbert <pe@ppy.sh>
2023-06-13 17:37:29 +0900

    Wait in `CVDisplayLink` callback until frame rendering has finished
    Turns out this will give more consistent results.

--- src/Veldrid/MTL/MTLGraphicsDevice.cs
+++ src/Veldrid/MTL/MTLGraphicsDevice.cs
@@ -45,6 +45,7 @@ namespace Veldrid.MTL
 
         private readonly IMTLDisplayLink _displayLink;
         private readonly AutoResetEvent _nextFrameReadyEvent;
+        private readonly AutoResetEvent _frameEndedEvent = new AutoResetEvent(true);
 
         public MTLDevice Device => _device;
         public MTLCommandQueue CommandQueue => _commandQueue;
@@ -234,6 +235,7 @@ namespace Veldrid.MTL
         private void OnDisplayLinkCallback()
         {
             _nextFrameReadyEvent.Set();
+            _frameEndedEvent.WaitOne();
         }
 
         public override TextureSampleCount GetSampleCountLimit(PixelFormat format, bool depthFormat)
@@ -335,6 +337,8 @@ namespace Veldrid.MTL
 
                 mtlSC.InvalidateDrawable();
             }
+
+            _frameEndedEvent.Set();
         }
 
         private protected override void UpdateBufferCore(DeviceBuffer buffer, uint bufferOffsetInBytes, IntPtr source, uint sizeInBytes)
